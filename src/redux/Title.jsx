3b7d62dfaa7e9d89bd1b481efbe8092a9f7d7df8
Andrejb596 <andrejb596@gmail.com>
2022-12-14 14:55:33 +0200

    feat: redux store

---
+++ src/redux/Title.jsx
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { inputText } from './actions';
+
+export default function Title() {
+  const dispatch = useDispatch();
+
+  const text = useSelector((state) => {
+    const { inputReducer } = state;
+    return inputReducer.text;
+  });
+
+  const handleChange = (e) => {
+    dispatch(inputText(e.target.value));
+  };
+
+  return (
+    <div className='card-title'>
+      <div className='card-title-top'>
+        <input type='text' onChange={handleChange} />
+      </div>
+        <p>{text}</p>
+    </div>
+  );
+}
